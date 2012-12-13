248bb743ef55d55fa003715f6e32e4ddf7459af7
Konrad Höffner <konradh.hoeffner@uni-leipzig.de>
2012-12-13 17:38:37 +0100

    deliverable script now directly writes to the turtle file. added property gk:identifier (same values as fp:deliverableIdentifier). added prefix definition to deliverable script output and replaced base prefix usages.

--- site/data/scripts/java/csv2rdf/src/Deliverables2Rdf.java
+++ site/data/scripts/java/csv2rdf/src/Deliverables2Rdf.java
@@ -1,10 +1,13 @@
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+
 import au.com.bytecode.opencsv.CSVReader;
 
 /**
@@ -17,10 +20,10 @@ public class Deliverables2Rdf
 	static Map<String,String> deliverableMap = null;
 	static {
 		deliverableMap = new HashMap<>();
-		deliverableMap.put("R",":Report");
-		deliverableMap.put("P",":Prototype");
-		deliverableMap.put("D",":Demonstrator");
-		deliverableMap.put("O",":Other");		
+		deliverableMap.put("R","fp:Report");
+		deliverableMap.put("P","fp:Prototype");
+		deliverableMap.put("D","fp:Demonstrator");
+		deliverableMap.put("O","fp:Other");		
 	}
 
 	static void printlnProperty(PrintStream out,String property, int object, boolean last)
@@ -36,8 +39,12 @@ public class Deliverables2Rdf
 	public static void main(String[] args) throws IOException
 	{		
 		File f = new File((args.length>0)?args[0]:"input/deliverables.csv");
-		PrintStream out = System.out;
-		
+//		PrintStream out = System.out;
+		PrintStream out = new PrintStream(new File("../../../deliverables.ttl"));
+		out.println("@prefix gk:      <http://geoknow.eu/> .");
+		out.println("@prefix fp:      <http://purl.org/research-fp#> .");
+		out.println("@prefix rdfs:    <http://www.w3.org/2000/01/rdf-schema#> .");
+		out.println("@prefix xsd:     <http://www.w3.org/2001/XMLSchema#> .");
 		try(CSVReader reader = new CSVReader(new FileReader(f)))
 		{
 			boolean first = true;
@@ -55,7 +62,7 @@ public class Deliverables2Rdf
 					String label = "\""+row[1].trim()+"\"@en";												
 					String nature = deliverableMap.get(row[3].trim());					
 					if(nature==null) throw new AssertionError("nature is null");
-					String disseminationLevel = ":"+row[4].trim();
+					String disseminationLevel = "fp:"+row[4].trim();
 					String deliveryDate = "\""+row[5].trim()+"\"^^xsd:nonNegativeInteger";					
 					
 					if(!first) {out.println();}
@@ -63,15 +70,18 @@ public class Deliverables2Rdf
 					out.println(uri+" a fp:Deliverable;");
 					printlnProperty(out,"rdfs:label",label,false);
 					printlnProperty(out,"fp:deliverableIdentifier","\""+row[0].trim()+"\"",false);
+					printlnProperty(out,"gk:identifier","\""+row[0].trim()+"\"",false);
 //					printlnProperty(out,":workpackageNumber",workpackageNumber,false);
-					printlnProperty(out,"fp:task",":Task"+workpackageNumber+"-"+taskNumber,false);
+					printlnProperty(out,"fp:task","gk:Task"+workpackageNumber+"-"+taskNumber,false);
 					printlnProperty(out,"fp:deliverableNature",nature,false);
 					printlnProperty(out,"fp:deliverableNumber",deliverableNumber,false);
 					printlnProperty(out,"fp:disseminationLevel",disseminationLevel,false);
 					printlnProperty(out,"fp:deliveryDate",deliveryDate,true);
+					
 				} 
-				catch(Throwable t) {System.err.println("Error with line :"+Arrays.toString(row)+": "+t);continue;}
+				catch(Throwable t) {System.err.println("Ignoring line :"+Arrays.toString(row)+": "+t);continue;}
 			}			
 		}
+		out.close();
 	}
 }
